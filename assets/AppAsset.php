570f3666e13d75c04ffb632d93e99752da6aad70
dervish <rai68@yandex.ru>
2017-10-26 21:13:45 +0300

    сделал начальный шаблон

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,12 +18,28 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/fonts.googleapis.com.css',
+        'css/ace.min.css',
+        'css/ace-skins.min.css',
+        'css/ace-rtl.min.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/jquery-ui.custom.min.js',
+        'js/jquery.ui.touch-punch.min.js',
+        'js/ace-elements.min.js',
+        'js/ace-extra.min.js',
+        'js/ace.min.js',
+
+    ];
+    public $jsOptions = [
+        //'position' => \yii\web\View::POS_HEAD, //влияет на работу фильтрации в таблицах представлений - виджет GridView!!!
     ];
     public $depends = [
+        'app\assets\IESupportAsset',
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
--- modules/main/views/error/view.php
+++ modules/main/views/error/view.php
@@ -10,12 +10,16 @@ if ($exception !== null) {
     $message = $exception->getMessage();
 }?>
 
-<div class="content">
-    <h2>Ой</h2>
-    <?php if($statusCode!=404) : ?>
-        <img src="/img/ops.jpg" alt="oops">
-    <?php else : ?>
-        <img src="/img/smile.png" alt="404">
-    <?php endif; ?>
-    <h2><?=$message ?></h2>
+<div class="page-content">
+    <div class="row">
+        <div class="col-md-3 col-md-offset-3">
+            <h2>Ой</h2>
+            <?php if($statusCode!=404) : ?>
+                <img src="/images/ops.jpg" alt="oops">
+            <?php else : ?>
+                <img src="/images/smile.png" alt="404">
+            <?php endif; ?>
+            <h2><?=$message ?></h2>
+        </div>
+    </div>
 </div>
